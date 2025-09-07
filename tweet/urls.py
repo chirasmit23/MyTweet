3b69afad1bfb0628f28590c02c1fe3f65f235211
chirasmit23 <149405361+chirasmit23@users.noreply.github.com>
2025-09-07 17:30:11 +0530

    Create urls.py

---
+++ tweet/urls.py
@@ -0,0 +1,17 @@
+from . import views
+from django.urls import path
+urlpatterns = [
+    path('', views.tweet_list, name="tweet_list"),
+    path('create/', views.tweet_create, name="tweet_create"),
+    path('<int:tweet_id>/edit/', views.tweet_edit, name="tweet_edit"),
+    path('<int:tweet_id>/delete/', views.tweet_delete, name='tweet_delete'),
+    path("switch-account/<int:profile_id>/", views.switch_account, name="switch_account"),
+    path('<int:tweet_id>/comment/', views.add_comment, name="add_comment"),
+    path('comment/<int:comment_id>/delete/', views.comment_delete, name="comment_delete"),
+    path('comment/<int:comment_id>/edit/', views.comment_edit, name="comment_edit"),
+    path('<int:tweet_id>/like/', views.like_tweet, name="like_tweet"),
+    path('<int:tweet_id>/dislike/', views.dislike_tweet, name="dislike_tweet"),
+ path("add/", views.add_account, name="add_account"),
+    path('register/', views.register, name='register'),
+    path('search/', views.search_view, name='search'),
+]
