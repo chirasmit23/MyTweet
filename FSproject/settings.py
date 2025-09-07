68f96c3439940681acbdd4cca568cb5a536678d0
chirasmit23 <149405361+chirasmit23@users.noreply.github.com>
2025-09-08 02:09:04 +0530

    Update settings.py

--- FSproject/settings.py
+++ FSproject/settings.py
@@ -135,9 +135,9 @@ STATICFILES_DIRS = [
 ]
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 LOGIN_URL='/accounts/login'
-LOGIN_REDIRECT_URL='/tweet/'
-
+LOGIN_REDIRECT_URL='/'
 LOGOUT_REDIRECT_URL='/'
+
 
 
 
